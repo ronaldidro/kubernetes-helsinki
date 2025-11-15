ec0723deb62c739e4af307788856a153d6b8b353
ronaldidro <rjdiazi10@gmail.com>
2025-11-15 17:55:16 -0500

    exercise 1.7 done

--- log_output/README.md
+++ log_output/README.md
@@ -1,6 +1,6 @@
 ## Log output app
 
-Deploy with `kubectl apply -f manifests/deployment.yaml`
+Deploy with `kubectl apply -f manifests`
 
 
 
--- log_output/index.js
+++ log_output/index.js
@@ -1,6 +1,28 @@
+import http from 'node:http';
+
 const randomId = crypto.randomUUID();
+let currentTimestamp = new Date().toISOString();
 
 setInterval(() => {
-  const timestamp = new Date().toISOString();
-  console.log(`${timestamp}: ${randomId}`);
+  currentTimestamp = new Date().toISOString();
+  console.log(`${currentTimestamp}: ${randomId}`);
 }, 5000);
+
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/status') {
+    const response = {
+      timestamp: currentTimestamp,
+      randomId: randomId
+    };
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(response));
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
