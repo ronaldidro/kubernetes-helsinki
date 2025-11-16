dabb0efa0cf4b1355658c6a5c8a9d04d6db29de4
ronaldidro <rjdiazi10@gmail.com>
2025-11-16 18:03:15 -0500

    exercise 1.11 done

--- log_output/README.md
+++ log_output/README.md
@@ -1,5 +1,7 @@
 ## Log output app
 
+Run `docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube` to create a directory
+
 Deploy with `kubectl apply -f manifests`
 
 Now visit `http://localhost:<published_port>`
--- ping_pong/index.js
+++ ping_pong/index.js
@@ -1,14 +1,17 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import fs from 'node:fs';
 
 const app = new Koa();
 const router = new Router();
 const PORT = process.env.PORT || 3000;
 
+const file = '/usr/src/app/data/log.txt';
 let counter = 0;
 
 router.get('/pingpong', (ctx) => {
   counter += 1;
+  fs.appendFileSync(file, `Ping / Pongs: ${counter}\n`);
   ctx.body = `pong ${counter}`;
 });
 
