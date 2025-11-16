58d950ceb2a50e1f38cdf3d086177512bf9d62f8
ronaldidro <rjdiazi10@gmail.com>
2025-11-16 10:12:14 -0500

    exercise 1.9 done

---
+++ ping_pong/README.md
@@ -0,0 +1,8 @@
+## Ping pong app
+
+Deploy with `kubectl apply -f manifests`
+
+Deploy the log ouput app too (shared ingress)
+
+Now visit `http://localhost:<published_port>/pingpong`
+
---
+++ ping_pong/index.js
@@ -0,0 +1,20 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+const PORT = process.env.PORT || 3000;
+
+let counter = 0;
+
+router.get('/pingpong', (ctx) => {
+  counter += 1;
+  ctx.body = `pong ${counter}`;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`Ping-pong app listening on port ${PORT}`);
+});
