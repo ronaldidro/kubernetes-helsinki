c37634952cee85b731f228381095c4594fa93441
ronaldidro <rjdiazi10@gmail.com>
2025-11-20 18:17:27 -0500

    exercise 2.7 done

--- ping_pong/index.js
+++ ping_pong/index.js
@@ -1,19 +1,45 @@
-import Koa from 'koa';
-import Router from 'koa-router';
+import Koa from "koa";
+import Router from "koa-router";
+import { Pool } from "pg";
 
 const app = new Koa();
 const router = new Router();
 const PORT = process.env.PORT || 3000;
 
-let counter = 0;
-
-router.get('/pingpong', (ctx) => {
-  counter += 1;
-  ctx.body = `pong ${counter}`;
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
 });
 
-router.get('/pings', (ctx) => {
-  ctx.body = { counter };
+async function initDB() {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS pings (
+      id SERIAL PRIMARY KEY,
+      value INT NOT NULL
+    );
+  `);
+
+  const res = await pool.query("SELECT * FROM pings LIMIT 1");
+
+  if (res.rows.length === 0) {
+    await pool.query("INSERT INTO pings (value) VALUES (0)");
+  }
+}
+
+initDB();
+
+router.get("/pingpong", async (ctx) => {
+  await pool.query("UPDATE pings SET value = value + 1 WHERE id = 1");
+  const res = await pool.query("SELECT value FROM pings WHERE id = 1");
+  ctx.body = `pong ${res.rows[0].value}`;
+});
+
+router.get("/pings", async (ctx) => {
+  const res = await pool.query("SELECT value FROM pings WHERE id = 1");
+  ctx.body = { counter: res.rows[0].value };
 });
 
 app.use(router.routes());
